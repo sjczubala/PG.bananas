b3f73f51d9e9a698a7c529bdfa785de04642559d
sjczubala <xbait04@gmail.com>
2021-04-14 14:05:34 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    int n,k[100];
+    cin >> n;
+    for(int i=0;i<100;i++){
+    while(cin >> k[i])continue;
+    }
+    for(int j=0;j<100;j++){
+        cout << k[j];
+    }
+    return 0;
+}
